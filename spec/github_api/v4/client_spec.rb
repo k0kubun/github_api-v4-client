94f89256c270820b42af8860a2761a87a647ae97
Takashi Kokubun <takashikkbn@gmail.com>
2017-08-25 00:03:40 +0900

    Initialize RSpec

--- spec/github_api/v4/client_spec.rb
+++ spec/github_api/v4/client_spec.rb
@@ -1,11 +1,5 @@
-require "spec_helper"
-
 RSpec.describe GithubApi::V4::Client do
-  it "has a version number" do
+  it 'has a version number' do
     expect(GithubApi::V4::Client::VERSION).not_to be nil
-  end
-
-  it "does something useful" do
-    expect(false).to eq(true)
   end
 end
